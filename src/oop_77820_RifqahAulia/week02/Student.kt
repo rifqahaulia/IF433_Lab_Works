6265462ec20b00a069dab187ff0e862b4bd2d2c6
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-02-16 18:22:24 +0700

    week02: add warning validation in init block

--- src/oop_77820_RifqahAulia/week02/Student.kt
+++ src/oop_77820_RifqahAulia/week02/Student.kt
@@ -5,5 +5,14 @@ class Student(
     val nim: String,
     var major: String
 ) {
-    // Body class kosong dulu
+
+    init {X
+        // Validasi sederhana: cek panjang NIM
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
 }
